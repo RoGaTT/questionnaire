f7d0d7136705bb213bcf753af34d69d337fc6136
RoGaTT <firestyl@yandex.ru>
2021-02-08 03:37:53 +0300

    fetch and send tests

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
-const API_BASE = 'http://employment-assistant.onti.actcognitive.org/api/questionnaries';
+const API_BASE = 'https://employment-assistant.onti.actcognitive.org/api/questionnaries';
 
 export default API_BASE;
